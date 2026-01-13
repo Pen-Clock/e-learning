b4084e64ec1a45867b3c1b3f6beccd8b958d1376
Ratanak Oudom Samnang <z5547618@ad.unsw.edu.au>
2026-01-13 17:09:10 +1100

    switch to v3 of tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,10 @@
-
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class", ".dark"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
